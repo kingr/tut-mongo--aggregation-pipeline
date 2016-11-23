7ef5efc4d84032d374ad8d7e4970adcd84dec713
kingr <kurt_farao@yahoo.com>
2016-11-23 11:15:41 +0200

    queries

--- queries.js
+++ queries.js
@@ -1,3 +1,14 @@
-/**
- * Created by kurtfarao on 2016/11/23.
- */
+db.companies.aggregate([
+    { $match : { founded_year : 2011 } },
+    { $sort : {name:-1}  },
+    { $project : { _id : 0, name : 1  } }
+])
+
+db.companies.aggregate([
+    { $match: { founded_year: { $gte: 2010 } } },
+    { $group: {
+        _id: "$founded_year",
+        companies: { $push: "$name" }
+    } },
+    { $sort: { "_id": 1 } }
+])
